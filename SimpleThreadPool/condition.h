a924ebc8ab70f22344b76c3c63c3617eaf3292d5
jiudianren <648087012@qq.com>
2018-12-02 22:37:04 +0800

    ds
    sd

---
+++ SimpleThreadPool/condition.h
@@ -0,0 +1,32 @@
+/*
+ * condition.h
+ *
+ *  Created on: 2018年12月2日
+ *      Author: Administrator
+ */
+
+#ifndef SIMPLETHREADPOOL_CONDITION_H_
+#define SIMPLETHREADPOOL_CONDITION_H_
+
+
+#include <pthread.h>
+
+//封装一个互斥量和条件变量作为状态
+typedef struct condition
+{
+    pthread_mutex_t pmutex;
+    pthread_cond_t pcond;
+}condition_t;
+
+//对状态的操作函数
+int condition_init(condition_t *cond);
+int condition_lock(condition_t *cond);
+int condition_unlock(condition_t *cond);
+int condition_wait(condition_t *cond);
+int condition_timedwait(condition_t *cond, const struct timespec *abstime);
+int condition_signal(condition_t* cond);
+int condition_broadcast(condition_t *cond);
+int condition_destroy(condition_t *cond);
+
+
+#endif /* SIMPLETHREADPOOL_CONDITION_H_ */
